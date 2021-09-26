051857062fc3f2f771f96405505d9c644bf9df5e
tyashin <tyashin@gmail.com>
2021-09-26 03:43:40 +0300

    Lesson-6 HW minor fix

--- lesson-6/leroymerlin/leroymerlin/settings.py
+++ lesson-6/leroymerlin/leroymerlin/settings.py
@@ -19,7 +19,7 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS = 16
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
